30704aaf7be848a588824a8d142bee93ea0d73f8
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-06-11 12:50:00 +0600

    added verifyjwt

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  console.log(authorization)
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+  console.log(token)
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -29,6 +51,13 @@ async function run() {
     const usersCollection = client.db("summerCameDb").collection("users")
     const classesCollection = client.db("summerCameDb").collection("classes")
     const cartsCollection = client.db("summerCameDb").collection("carts")
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({token})
+    })
+    
 
     // user related apis
     app.get('/users', async (req, res) => {
@@ -79,12 +108,18 @@ async function run() {
     })
 
     // cart collection
-    app.get('/carts', async (req, res) => {
+    app.get('/carts',verifyJWT, async (req, res) => {
       const email = req.query.email;
       console.log(email)
       if (!email) {
         res.send([])
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !==decodedEmail){
+        return res.send(403).send({ error: true, message: 'forbidden access'})
+      }
+
       const query = { email: email };
       const result = await cartsCollection.find(query).toArray();
       res.send(result)
