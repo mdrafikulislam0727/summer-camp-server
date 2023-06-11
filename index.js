51efc85f2ec25bf116dc9c19328c05895a2cc729
mdrafikulislam <mdrafikulislam0727@gmail.com>
2023-06-11 10:48:40 +0600

    update instructor

--- index.js
+++ index.js
@@ -47,13 +47,25 @@ async function run() {
       const result = await usersCollection.insertOne(user)
       res.send(result);
     })
-
+// admin role set
     app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) }
       const updateDoc = {
         $set: {
           role: "admin"
+        },
+      };
+      const result =await usersCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+    // instructor role set
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: "instructor"
         },
       };
       const result =await usersCollection.updateOne(filter, updateDoc)
